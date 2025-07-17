9e53114d426f01573995f232e2df6a8eeca9ebb5
meraj <merajpiri83@gmail.com>
2025-07-17 15:53:22 +0330

    Add Most Sell

---
+++ app/components/mostSell.tsx
@@ -0,0 +1,71 @@
+import Image from "next/image";
+import HolderImage from "@/public/dev/holder.svg";
+import MedalStarIcon from "@/components/icons/home/medalStar";
+import HeartIcon from "@/components/icons/home/heart";
+import StarIcon from "@/components/icons/home/star";
+
+
+
+const Product = () => {
+    return (
+        <div className="border-[1px] border-[#CBCBCB] rounded-[16px] p-[15px]">
+            <div className="w-full h-[245px] relative bg-[#A1A1A1] rounded-[8px]">
+                <Image 
+                    src={HolderImage}
+                    alt="holder image"
+                    fill={true}
+                    sizes="100%"
+                    className="rounded-[8px]"
+                    style={{objectFit : "cover",objectPosition : "center"}}
+                />
+            </div>
+            <div className="flex items-center justify-between mt-2">
+                <p className="text-[#3D3D3D] font-semibold text-[18px]">هولدر موبایل</p>
+                <div className="size-[24px]">
+                    <HeartIcon />
+                </div>
+            </div>
+            <p className="mt-2 text-[#3D3D3D] text-[16px] text-justify">
+                دارای رنگ بندی، مناسب گوشی های 5 تا 6 اینچی</p>
+
+            <div className="flex items-between justify-between mt-2">
+                <div className="flex items-center justify-center px-[8px] py-[4px] bg-[#E9F3FB] rounded-[4px]">
+                    <div className="size-[20px]">
+                        <StarIcon />
+                    </div>
+                    <p className="text-[#3D3D3D]">3.2</p>
+                </div>
+                <p className="text-[#3D3D3D] font-semibold text-[18px]">۱۵۰,۰۰۰ تومان</p>
+            </div>
+        </div>
+    )
+}
+
+const MostSell = () => {
+
+    const items = [1,2,3,4,5,6,7,8];
+
+    return (
+        <div className="my-[30px] mx-[80px] [direction:rtl]">
+            <div className="flex items-center justify-between">
+                <div className="flex items-center justify-center gap-[8px]">
+                    <div className="size-[40px]">
+                        <MedalStarIcon />
+                    </div>
+                    <p className="text-[#3D3D3D] font-bold text-[24px]">پر فروش ترین ها</p>
+                </div>
+                <div className="w-[70%] bg-[#E9E9E9] h-[1px]"></div>
+                <p className="text-[#2196F3] text-[16px]">مشاهده بیشتر</p>
+            </div>
+            <div className="my-6 grid grid-cols-4 gap-[25px]">
+                {
+                    items.map((product) => {
+                        return (
+                            <Product key={product} />
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+};export default MostSell;
--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import "@/app/components/style.css";
 
 const Slider = dynamic(() => import("@/app/components/slider"),{ssr : true});
 const Stats = dynamic(() => import("@/app/components/stats"),{ssr : true});
+const MostSell = dynamic(() => import("@/app/components/mostSell"),{ssr : true});
 
 const Page = () => {
     return (
         <>
             <Slider />
             <Stats />
+            <MostSell />
         </>
     )
 };export default Page;
