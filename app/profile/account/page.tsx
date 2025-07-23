91bb1ad7b7a39dff95c0107a7ca2cf11ada57793
meraj <merajpiri83@gmail.com>
2025-07-23 21:44:37 +0330

    s

--- app/profile/account/page.tsx
+++ app/profile/account/page.tsx
@@ -3,8 +3,28 @@
 
 const Page = () => {
     return (
-        <div>
-            info
+        <div className="border-[1px] border-[#EDEDED] rounded-[16px] p-[25px]">
+            <div className="pb-4 border-b border-[#EDEDED]">
+                <p className="text-[#3D3D3D] font-bold text-[24px]">اطلاعات حساب کاربری</p>
+            </div>
+            <div className="mx-[80px] my-[30px] grid grid-cols-2 gap-[25px]">
+                <div className="col-span-1">
+                    <p className="text-[#CBCBCB] text-[14px]">نام و نام خانوادگی </p>
+                    <input 
+                        type="text"
+                        className="w-full text-[16px] outline-none rounded-[8px] py-[6px] px-[8px]
+                            border border-[#CBCBCB] mt-2"
+                    />
+                </div>
+                <div className="col-span-1">
+                    <p className="text-[#CBCBCB] text-[14px]">آدرس ایمیل</p>
+                    <input 
+                        type="text"
+                        className="w-full text-[16px] outline-none rounded-[8px] py-[6px] px-[8px]
+                            border border-[#CBCBCB] mt-2"
+                    />
+                </div>
+            </div>
         </div>
     )
 };export default Page;
