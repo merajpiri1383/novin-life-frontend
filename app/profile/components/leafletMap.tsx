47422018393906f4d789e6df3b396baf772142b7
meraj <merajpiri83@gmail.com>
2025-07-24 23:05:15 +0330

    Add Address Section In Profile

--- app/profile/components/leafletMap.tsx
+++ app/profile/components/leafletMap.tsx
@@ -1,13 +1,41 @@
-'use client';                            // ← must be first line
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+'use client';
 import { FC } from 'react';
+import { useEffect } from 'react';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-control-geocoder/dist/Control.Geocoder.css';
+import L from 'leaflet';
+import 'leaflet-control-geocoder'
 
 type Props = {
-    height ?: string,
+    height?: string,
+    show_search ?: boolean,
 }
 
-const LeafletMap : FC<Props> = ({ height = '400px' }) => {
+
+const GeoCoderControl = () => {
+    const map = useMap();
+
+    useEffect(() => {
+        if (!map) null;
+        const geocoder = (L.Control as any).geocoder({
+            defaultMarkGeocode: true,
+        })
+            .on('markgeocode', function (e: any) {
+                const bbox = e.geocode.bbox;
+                const bounds = L.latLngBounds(bbox);
+                map.fitBounds(bounds);
+            })
+            .addTo(map);
+
+        return () => {
+            map.removeControl(geocoder);
+        };
+    }, [map]);
+    return null;
+}
+
+const LeafletMap: FC<Props> = ({ height = '400px',show_search = false }) => {
     return (
         <MapContainer
             center={[35.6892, 51.3890]}
@@ -18,6 +46,9 @@ const LeafletMap : FC<Props> = ({ height = '400px' }) => {
             <Marker position={[35.6892, 51.3890]}>
                 <Popup>تهران، ایران</Popup>
             </Marker>
+            {
+                show_search && <GeoCoderControl />
+            }
         </MapContainer>
     )
 }; export default LeafletMap;
