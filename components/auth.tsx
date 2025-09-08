7b9236335655c875d4b8883f713125cde4270fec
meraj <merajpiri83@gmail.com>
2025-09-08 08:54:54 +0330

    Debug #4

--- components/auth.tsx
+++ components/auth.tsx
@@ -1,42 +1,42 @@
 "use client"
-// import { GetCompany } from "@/app/api/company";
-// import { useQuery } from "@tanstack/react-query";
-import { FC } from "react";
-// import { CompanyType } from "@/components/types";
-// import { useCompanyStore } from "@/lib/store/company";
+import { GetCompany } from "@/app/api/company";
+import { useQuery } from "@tanstack/react-query";
+import { FC, useEffect } from "react";
+import { CompanyType } from "@/components/types";
+import { useCompanyStore } from "@/lib/store/company";
 
 
 
 const Auth: FC<{ children: React.ReactNode }> = ({ children }) => {
 
-    // const { data }: { data: CompanyType } = useQuery({
-    //     queryKey: ["company"],
-    //     queryFn: GetCompany,
-    // });
+    const { data }: { data: CompanyType } = useQuery({
+        queryKey: ["company"],
+        queryFn: GetCompany,
+    });
 
-    // const companyStore = useCompanyStore();
+    const { setFooter, setHeader, setTheme } = useCompanyStore();
 
 
-    // useEffect(() => {
+    useEffect(() => {
 
-    //     try {
-    //         if (data?.output.footer) {
-    //             companyStore.setFooter(data?.output.footer);
-    //         }
-    //     } catch { }
+        try {
+            if (data?.output.footer) {
+                setFooter(data?.output.footer);
+            }
+        } catch { }
 
-    //     try {
-    //         if (data?.output.header) {
-    //             companyStore.setHeader(data.output.header);
-    //         }
-    //     } catch { }
+        try {
+            if (data?.output.header) {
+                setHeader(data.output.header);
+            }
+        } catch { }
 
-    //     try {
-    //         if (data?.output.theme) {
-    //             companyStore.setTheme(data.output.theme);
-    //         }
-    //     } catch {}
-    // }, [data]);
+        try {
+            if (data?.output.theme) {
+                setTheme(data.output.theme);
+            }
+        } catch {}
+    }, [data, setFooter, setHeader, setTheme]);
 
     return (
         children
